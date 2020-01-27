0a8d0e6737fade5b0e2687050ba3e79aa56e7153
xenocidewiki <xenocidebacon@gmail.com>
2020-01-27 00:37:44 +0000

    Cleanup

--- assaultcubehack/hacks.cpp
+++ assaultcubehack/hacks.cpp
@@ -11,25 +11,17 @@ void Hackeroni::aimbot()
 
 	get_closest_player(local_player, target);
 
-	Vector3D yoinked;
-	yoinked.x = 0;
-	yoinked.y = 0;
-	yoinked.z = 0;
 	if (target->health > 0) {
-		yoinked.x = target->player_pos.x - local_player->player_pos.x;
-		yoinked.y = target->player_pos.y - local_player->player_pos.y;
-		yoinked.z = target->player_pos.z - local_player->player_pos.z;
 
-		float mag = sqrt(pow(yoinked.x, 2) + pow(yoinked.y, 2));
+		local_player->viewangles.y = 180 * asinf((target->player_pos.z - local_player->player_pos.z) / sqrt(pow((target->player_pos.x - local_player->player_pos.x), 2) + pow((target->player_pos.y - local_player->player_pos.y), 2))) / math_pi;
+		local_player->viewangles.x = 90 + 180 * atanf((target->player_pos.y - local_player->player_pos.y) / (target->player_pos.x - local_player->player_pos.x)) / math_pi;
 
-		local_player->viewangles.y = 180 * asinf(yoinked.z / mag) / math_pi;
-		local_player->viewangles.x = 90 + 180 * atanf(yoinked.y / yoinked.x) / math_pi;
-
-		if (yoinked.x < 0)
+		if ((target->player_pos.x - local_player->player_pos.x) < 0)
 			local_player->viewangles.x += 180;
 	}
 }
 
+//I think this is dumb, perhaps checking if player is visible and then just grabbing the closest one by euclidean distance is better lol
 void Hackeroni::get_closest_player(Player* local_player, Player* &closest_player_out)
 {
 	float target_angle	= 999999;
@@ -47,14 +39,9 @@ void Hackeroni::get_closest_player(Player* local_player, Player* &closest_player
 		if (current_entity->health < 0 || current_entity->health > 100)
 			continue;
 
-		Vector3D yoinked;
+		float new_angle = 90 + 180 * atanf((current_entity->player_pos.x - local_player->player_pos.x) / (current_entity->player_pos.y - local_player->player_pos.y)) / math_pi;
 
-		yoinked.x = current_entity->player_pos.x - local_player->player_pos.x;
-		yoinked.y = current_entity->player_pos.y - local_player->player_pos.y;
-
-		float new_angle = 90 + 180 * atanf(yoinked.y / yoinked.x) / 3.14159265358979323846;
-
-		if (yoinked.x < 0)
+		if ((current_entity->player_pos.x - local_player->player_pos.x) < 0)
 			new_angle += 180;
 
 		if (new_angle < target_angle) {
@@ -74,10 +61,11 @@ void Hackeroni::world_to_screen(Vector3D enemy_coordinates, Vector2D &pixel_coor
 	for (int i = 0; i < 16; i++)
 		matrix.push_back(*(matrix_ptr + i));
 
+	//you can read this from an offset REEEEE
 	float screen_x = 1024 / 2.0F;
 	float screen_y = 768 / 2.0F;
 
-	//could be made into a function but i honestly can't be fucked to do it
+	//could be made into a function but i honestly can't be fucked to do it ex deeee
 	float x = enemy_coordinates.x * matrix[0] + enemy_coordinates.y * matrix[4] + enemy_coordinates.z * matrix[8] + matrix[12];
 	float y = enemy_coordinates.x * matrix[1] + enemy_coordinates.y * matrix[5] + enemy_coordinates.z * matrix[9] + matrix[13];
 	float z = enemy_coordinates.x * matrix[2] + enemy_coordinates.y * matrix[6] + enemy_coordinates.z * matrix[10] + matrix[14];
@@ -106,6 +94,7 @@ void Hackeroni::draw_esp()
 
 		Vector2D screen_head, screen_foot;
 
+		//Seems broken, I believe i should add a certain check, if i flip myself 180 then this shit draws esp on the other side of the map where there are no players, lulw
 		world_to_screen(current_entity->player_pos, screen_head);
 		world_to_screen(current_entity->foot_pos, screen_foot);
 
